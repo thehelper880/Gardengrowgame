44b6d835046585f81645c31f1b5bbe71c732008c
thehelper880 <clementkyle32@gmail.com>
2025-06-10 22:40:31 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.0/firebas
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDI9r6htJkhDgDRleTHFU98eHL4bmF5_1U",
+  apiKey: "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",
   authDomain: "gardengrowgame.firebaseapp.com",
   projectId: "gardengrowgame",
   storageBucket: "gardengrowgame.appspot.com",
